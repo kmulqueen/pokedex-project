86712eb86d02e99c5411d5eb5ceba2b1e11f476f
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-01-31 20:53:56 -0700

    Preparing for Heroku deployment.

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
 import { Provider } from "react-redux";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
