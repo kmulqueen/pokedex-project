0c38bc6e886ec2f4683dd230aff322656b485d30
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-01-31 21:30:47 -0700

    Added express static build instructions to server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,16 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  // Any route that is not an api route points to the index.html file in the static build folder
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
