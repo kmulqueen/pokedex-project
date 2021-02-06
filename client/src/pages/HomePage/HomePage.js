bc19eaa81ff752b1c632a928c3552efda112f4e5
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-02-05 21:25:16 -0700

    Added basic filter skeleton to homepage to filter by pokemon types and/or weaknesses. If a T or W is clicked it will add to the filter state and the background color will change. If it is removed it will be removed from the filter state and the background color will go back to normal.

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -9,8 +9,31 @@ const HomePage = () => {
   const dispatch = useDispatch();
   const { pokemon, loading } = useSelector((state) => state.getAllPokemon);
 
+  const pokemonTypes = [
+    "Bug",
+    "Dark",
+    "Dragon",
+    "Electric",
+    "Fairy",
+    "Fighting",
+    "Fire",
+    "Flying",
+    "Ghost",
+    "Grass",
+    "Ground",
+    "Ice",
+    "Normal",
+    "Poison",
+    "Psychic",
+    "Rock",
+    "Steel",
+    "Water",
+  ];
+
   const [list, setList] = useState([]);
   const [search, setSearch] = useState("");
+  const [filterType, setFilterType] = useState([]);
+  const [filterWeakness, setFilterWeakness] = useState([]);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -38,6 +61,52 @@ const HomePage = () => {
           setList([]);
         }
       }
+    }
+  };
+
+  const handleTypeClick = (e) => {
+    e.stopPropagation();
+    const type = e.target.getAttribute("name");
+    const classes = e.target.classList;
+    const activeClass = "filter-li__option--active";
+    // Check if type already exists in filterType array
+    if (filterType.includes(type)) {
+      // Remove type from array
+      const oldArr = [...filterType];
+      const updatedArr = oldArr.filter(
+        (name) => name.toLowerCase() !== type.toLowerCase()
+      );
+      setFilterType([...updatedArr]);
+      // Remove active class
+      classes.remove(activeClass);
+    } else {
+      // Add type to array
+      setFilterType([...filterType, type]);
+      // Add active class
+      classes.add(activeClass);
+    }
+  };
+
+  const handleWeaknessClick = (e) => {
+    e.stopPropagation();
+    const weakness = e.target.getAttribute("name");
+    const classes = e.target.classList;
+    const activeClass = "filter-li__option--active";
+    // Check if weakness already exists in filterWeakness array
+    if (filterWeakness.includes(weakness)) {
+      // Remove weakness from array
+      const oldArr = [...filterWeakness];
+      const updatedArr = oldArr.filter(
+        (name) => name.toLowerCase() !== weakness.toLowerCase()
+      );
+      setFilterWeakness([...updatedArr]);
+      // Remove active class
+      classes.remove(activeClass);
+    } else {
+      // Add weakness to array
+      setFilterWeakness([...filterWeakness, weakness]);
+      // Add active class
+      classes.add(activeClass);
     }
   };
 
@@ -69,6 +138,29 @@ const HomePage = () => {
           Search
         </button>
       </form>
+      <ul className="form-advanced__filter">
+        {pokemonTypes.map((type) => (
+          <li className="filter-li" key={type}>
+            <span className={`filter-li__label filter-li__label--${type}`}>
+              {type}
+            </span>
+            <span
+              className="filter-li__option filter-li__type"
+              name={type}
+              onClick={handleTypeClick}
+            >
+              T
+            </span>
+            <span
+              className="filter-li__option filter-li__weakness"
+              name={type}
+              onClick={handleWeaknessClick}
+            >
+              W
+            </span>
+          </li>
+        ))}
+      </ul>
 
       <AllPokemonView
         pokemon={list.length ? list : pokemon}
